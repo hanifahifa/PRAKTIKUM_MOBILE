1041066eebe35da97a53e2e2f6fb4a5a8d7694d5
hanifah <hanifahutamisunaryo@gmail.com>
2025-09-17 10:12:31 +0700

    new push percobaan 3

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,54 @@ void main() {
   runApp(const MyApp());
 }
 
-Container kotakUji(Color warna) {
-  return Container(width: 75, height: 75, color: warna);
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contoh SizedBox',
+      title: 'Demo Card',
       home: Scaffold(
         appBar: AppBar(
+          title: const Text('Card Demo'),
           backgroundColor: Colors.amber,
-          title: const Text('Contoh SizedBox'),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              kotakUji(Colors.amber),
-              const Spacer(flex: 1),
-              kotakUji(Colors.green),
-              const Spacer(flex: 2),
-              kotakUji(Colors.blue),
-            ],
+          child: Card(
+            margin: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const ListTile(
+                  leading: Icon(Icons.location_pin, color: Colors.red),
+                  title: Text('Taman Nasional Bromo Tengger Semeru'),
+                  subtitle: Text('Jawa Timur, Indonesia'),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Colors.blue),
+                        ),
+                        child: const Icon(Icons.map),
+                      ),
+                      const SizedBox(width: 10),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Colors.blue),
+                        ),
+                        child: const Icon(Icons.phone),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
