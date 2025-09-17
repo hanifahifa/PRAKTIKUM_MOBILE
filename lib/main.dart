6b266953884c1d2d7e9c6dee72bd6edebd130ebf
hanifah <hanifahutamisunaryo@gmail.com>
2025-09-17 13:27:39 +0700

    new push listView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo GridView.builder',
+      title: 'Demo ListView.builder',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Demo GridView.builder'),
+      home: const MyHomePage(title: 'Demo ListView.builder'),
     );
   }
 }
@@ -65,43 +65,48 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.amber, title: Text(widget.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            childAspectRatio: 1.25,
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-          ),
-          itemCount: dataBerita.length,
-          itemBuilder: (context, index) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: GridTile(
-                footer: SizedBox(
-                  height: 65,
-                  child: GridTileBar(
-                    backgroundColor: Colors.black26.withAlpha(175),
-                    title: Text(
-                      dataBerita[index]['title'] ?? 'Tanpa judul',
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ),
-                child: Image(
-                  image: NetworkImage(
-                    dataBerita[index]['image'] ??
-                        'http://cdn.pixabay.com/photo/2018/03/17/20/51/white-buildings-3235135__340.jpg',
-                  ),
-                  fit: BoxFit.cover,
+      body: ListView.builder(
+        itemCount: dataBerita.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  width: 1,
                 ),
               ),
-            );
-          },
-        ),
-      ), // sementara, akan diganti GridView.builder
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: ListTile(
+                  title: Text(
+                    dataBerita[index]['title'] ?? "Tidak ada judul",
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    dataBerita[index]['published_at'] ?? "Tidak ada data",
+                    maxLines: 1,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  leading: Image.network(
+                    dataBerita[index]['image'] ??
+                        'http://cdn.pixabay.com/photo/2018/03/17/20/51/white-buildings-3235135__340.jpg',
+                    fit: BoxFit.cover,
+                    width: 100,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
