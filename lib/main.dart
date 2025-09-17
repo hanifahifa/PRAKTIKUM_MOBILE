85a5fc9ccea6c6000cf3e390fde24089a49a34f9
hanifah <hanifahutamisunaryo@gmail.com>
2025-09-17 13:14:33 +0700

    new push GridView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,86 +12,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo ListView',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Demo ListView'),
-          backgroundColor: Colors.amber,
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: [
-            listTile(
-              Colors.blueAccent.shade400,
-              Colors.greenAccent.shade400,
-              'Kehadiran',
-              'Presensi kehadiran kuliah',
-              'assets/icon/boy.png',
-            ),
-            listTile(
-              Colors.greenAccent.shade400,
-              Colors.blueAccent.shade400,
-              'Jadwal',
-              'Jadwal perkuliahan',
-              'assets/icon/timetable.png',
-            ),
-            listTile(
-              Colors.yellowAccent.shade400,
-              Colors.redAccent.shade400,
-              'Tugas',
-              'Tugas perkuliahan di luar kelas',
-              'assets/icon/homeschooling.png',
-            ),
-            listTile(
-              Colors.redAccent.shade400,
-              Colors.amberAccent.shade400,
-              'Pengumuman',
-              'Informasi terkait perkuliahan',
-              'assets/icon/checklist.png',
-            ),
-            listTile(
-              Colors.purpleAccent.shade400,
-              Colors.tealAccent.shade400,
-              'Nilai',
-              'Nilai ujian dan tugas',
-              'assets/icon/warning.png',
-            ),
-            listTile(
-              Colors.tealAccent.shade400,
-              Colors.purpleAccent.shade400,
-              'Catatan',
-              'Pengingat kegiatan perkuliahan',
-              'assets/icon/pencil.png',
-            ),
-          ],
-        ),
+      title: 'Demo GridView.builder',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Demo GridView.builder'),
     );
   }
 }
 
-Container listTile(
-  Color warna,
-  Color warnaAvatar,
-  String judul,
-  String subjudul,
-  String gambar,
-) {
-  return Container(
-    padding: const EdgeInsets.symmetric(vertical: 5),
-    child: ListTile(
-      tileColor: warna,
-      title: Text(
-        judul,
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
-      subtitle: Text(subjudul, style: const TextStyle(fontSize: 16)),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: warnaAvatar,
-        child: Image.asset(gambar, width: 35, height: 35),
-      ),
-      trailing: const Icon(Icons.star, color: Colors.orangeAccent),
-    ),
-  );
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // List untuk menyimpan data berita
+  List dataBerita = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _ambilData();
+  }
+
+  // Method request data API
+  Future _ambilData() async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://jakpost.vercel.app/api/category/business/tech'),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          dataBerita = data['posts'];
+        });
+      } else {
+        throw Exception('Gagal load data');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.amber, title: Text(widget.title)),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 1.25,
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemCount: dataBerita.length,
+          itemBuilder: (context, index) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: GridTile(
+                footer: SizedBox(
+                  height: 65,
+                  child: GridTileBar(
+                    backgroundColor: Colors.black26.withAlpha(175),
+                    title: Text(
+                      dataBerita[index]['title'] ?? 'Tanpa judul',
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                child: Image(
+                  image: NetworkImage(
+                    dataBerita[index]['image'] ??
+                        'http://cdn.pixabay.com/photo/2018/03/17/20/51/white-buildings-3235135__340.jpg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
+      ), // sementara, akan diganti GridView.builder
+    );
+  }
 }
