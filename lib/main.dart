cf5c77b9d0a18487c47127cec613ec86956f7630
hanifah <hanifahutamisunaryo@gmail.com>
2025-10-07 21:02:16 +0700

    modul9

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,106 @@
 import 'package:flutter/material.dart';
-import 'LikeButtonPage.dart';
-import 'CounterPage.dart';
+import 'package:flutter/services.dart';
+import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late ValueNotifier<double> _valueNotifier;
+  late double counter;
+
+  void incrementCounter() {
+    setState(() {
+      if (counter < 33) {
+        counter++;
+        _valueNotifier.value = (counter / 33) * 100;
+      }
+    });
+  }
+
+  void resetCounter() {
+    setState(() {
+      counter = 0.0;
+      _valueNotifier.value = (counter / 33) * 100;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _valueNotifier = ValueNotifier(0.0);
+    counter = 0.0;
+  }
+
+  @override
+  void dispose() {
+    _valueNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const CounterPage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 119, 210, 145),
+        primarySwatch: Colors.amber,
+      ),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 119, 210, 145),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('${counter.round()}', style: const TextStyle(fontSize: 50)),
+              SimpleCircularProgressBar(
+                progressColors: const [Colors.amber, Colors.red],
+                size: 300,
+                progressStrokeWidth: 20,
+                backStrokeWidth: 10,
+                backColor: Colors.black12,
+                maxValue: 100,
+                animationDuration: 0,
+                valueNotifier: _valueNotifier,
+                onGetText: (value) {
+                  return Text(
+                    '${(value.toInt() / 3).round()}',
+                    style: const TextStyle(fontSize: 170),
+                  );
+                },
+              ),
+              const SizedBox(height: 50),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(50)),
+                child: Material(
+                  child: InkWell(
+                    onTap: incrementCounter,
+                    child: Container(
+                      decoration: const BoxDecoration(color: Colors.white),
+                      child: const Icon(Icons.fingerprint, size: 125),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: resetCounter,
+          child: const Icon(Icons.refresh_outlined),
+        ),
+      ),
     );
   }
 }
-
-
-
-
